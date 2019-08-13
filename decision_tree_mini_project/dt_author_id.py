a9d1ba167968422a00dfb646733b78dd2d571e8d
joshua-nicholson <joshwnicholson@gmail.com>
2019-08-12 22:59:21 -0400

    Completed decision tree mini-project

--- decision_tree_mini_project/dt_author_id.py
+++ decision_tree_mini_project/dt_author_id.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 
 """ 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Naive Bayes Classifier to identify emails by their authors
-    
-    authors and labels:
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
     Sara has label 0
     Chris has label 1
 """
@@ -14,9 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,16 +21,13 @@ from sklearn.metrics import accuracy_score
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
-clf  = GaussianNB()
-t0 = time()
+clf = DecisionTreeClassifier(min_samples_split=40)
 clf.fit(features_train, labels_train)
-print("training time:", round(time()-t0, 3), "s")
-prediction = clf.predict(features_test)
+prediction = (clf.predict(features_test))
 print(accuracy_score(labels_test, prediction))
+
 #########################################################
 
 
